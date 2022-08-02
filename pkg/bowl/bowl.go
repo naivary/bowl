ec3e9afda2be7cd12d25684093e3c26348122c72
Mustafa Hussaini <mustafa.hussaini@etomer.com>
2022-08-02 12:17:00 +0200

    staging

--- pkg/bowl/bowl.go
+++ pkg/bowl/bowl.go
@@ -67,18 +67,18 @@ func (b *Bowl[T]) Get() T {
 	return b.new()
 }
 
-func (b *Bowl[T]) incrementSize(i int32) {
-	atomic.AddInt32(&b.size, i)
-}
-
-func (b *Bowl[T]) decrementSize(i int32) {
-	atomic.AddInt32(&b.size, i)
-}
-
 func (b *Bowl[T]) Size() int32 {
 	return b.size
 }
 
 func (b *Bowl[T]) Max() int32 {
 	return b.max
+}
+
+func (b *Bowl[T]) incrementSize(i int32) {
+	atomic.AddInt32(&b.size, i)
+}
+
+func (b *Bowl[T]) decrementSize(i int32) {
+	atomic.AddInt32(&b.size, i)
 }
